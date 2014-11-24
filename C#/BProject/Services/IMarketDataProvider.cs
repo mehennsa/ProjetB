39d1c03a6b13059a5a02a1d78189a961cb34e954
pelletgu <pelletgu@gmail.com>
2014-11-24 21:08:56 +0100

    voici les classes

---
+++ C#/BProject/Services/IMarketDataProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APIFiMag;
+using APIFiMag.Exporter;
+using APIFiMag.Datas;
+using Engine;
+
+namespace Services
+{
+    public class IMarketDataProvider
+    {
+        private DataProjDataContext db;
+
+
+        public IMarketDataProvider()
+        {
+            db = new DataProjDataContext();
+        }
+
+        public Dictionary<IQuote, Curve> getLastMarketData(string ticker, List<DateTime> dates)
+        {
+            return null;
+        }
+
+        public void RefreshDataMarket()
+        {
+        }
+    }
+}
