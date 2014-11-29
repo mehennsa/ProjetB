a856ffeea41f0f6982778d1082a61c3dc03508c2
mehennsa <mehenni.samy@gmail.com>
2014-11-29 15:00:42 +0100

    Mise en place des services

---
+++ C#/BProject/Services/EstimatorCreator/IEstimatorCreator.cs
@@ -0,0 +1,16 @@
+﻿using Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.EstimatorCreator
+{
+    public interface IEstimatorCreator
+    {
+        Engine.Estimator CreateEstimator(string estimatorName);
+
+        IList<Engine.Estimator> CreateEstimators(IList<string> estimatorNames);
+    }
+}
---
+++ SQL/Estimators/ExempleInsertEstimators.sql
@@ -0,0 +1,11 @@
+USE [ProjetB]
+GO
+
+INSERT INTO Estimator (Name, [Full Name], [Assembly])
+     VALUES
+           ('MyEstimatorName',
+		   'MyEstimatorNamespace.MyEstimatorClassName',
+		   'MyEstimatorAssembly');
+GO
+
+
