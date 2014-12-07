268ac4f8c106c26cf750168bee00b702c599f24c
FuchsG <guillaumfuchs@gmail.com>
2014-12-07 18:28:39 +0100

    EMA updated

--- C#/BProject/UnitTest/EstimatorCreator/EstimatorCreatorHelperUT.cs
+++ C#/BProject/UnitTest/EstimatorCreator/EstimatorCreatorHelperUT.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Ec = Services.EstimatorCreator;
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Ec = Services.EstimatorCreator;
 
-namespace UnitTest.EstimatorCreator
-{
-    [TestClass]
-    public class EstimatorCreatorHelperUT
-    {
-        private const string EstimatorName = "MockEstimator";
+//namespace UnitTest.EstimatorCreator
+//{
+//    [TestClass]
+//    public class EstimatorCreatorHelperUT
+//    {
+//        private const string EstimatorName = "MockEstimator";
 
-        private const string AssemblyName = "UnitTest";
+//        private const string AssemblyName = "UnitTest";
 
-        private const string FullName = "UnitTest.EstimatorCreator.MockEstimator";
+//        private const string FullName = "UnitTest.EstimatorCreator.MockEstimator";
 
-        [TestMethod]
-        public void CreateEstimatorValidTest()
-        {
-            var estimator = Ec.EstimatorCreatorHelper.CreateEstimatorFromInfo(AssemblyName, FullName);
+//        [TestMethod]
+//        public void CreateEstimatorValidTest()
+//        {
+//            var estimator = Ec.EstimatorCreatorHelper.CreateEstimatorFromInfo(AssemblyName, FullName);
 
-            Assert.IsNotNull(estimator);
+//            Assert.IsNotNull(estimator);
 
-            Assert.AreEqual(typeof(MockEstimator), estimator.GetType());
-        }
+//            Assert.AreEqual(typeof(MockEstimator), estimator.GetType());
+//        }
 
-        [TestMethod]
-        public void CreateEstimatorWithNullParametersTest()
-        {
-            var estimatorWithoutAssembly = Ec.EstimatorCreatorHelper.CreateEstimatorFromInfo(null, FullName);
+//        [TestMethod]
+//        public void CreateEstimatorWithNullParametersTest()
+//        {
+//            var estimatorWithoutAssembly = Ec.EstimatorCreatorHelper.CreateEstimatorFromInfo(null, FullName);
 
-            Assert.IsNull(estimatorWithoutAssembly);
+//            Assert.IsNull(estimatorWithoutAssembly);
 
-            var estimatorWithoutFullName = Ec.EstimatorCreatorHelper.CreateEstimatorFromInfo(AssemblyName, null);
+//            var estimatorWithoutFullName = Ec.EstimatorCreatorHelper.CreateEstimatorFromInfo(AssemblyName, null);
 
-            Assert.IsNull(estimatorWithoutFullName);
-        }
+//            Assert.IsNull(estimatorWithoutFullName);
+//        }
 
-        [TestMethod]
-        [ExpectedException(typeof(Exception), "this estimator is not defined")]
-        public void CreateNotExistingEstimator()
-        {
-            var wrongEstimator = Ec.EstimatorCreatorHelper.CreateEstimatorFromInfo("MyAssembly", "MyFullName");
-        }
+//        [TestMethod]
+//        [ExpectedException(typeof(Exception), "this estimator is not defined")]
+//        public void CreateNotExistingEstimator()
+//        {
+//            var wrongEstimator = Ec.EstimatorCreatorHelper.CreateEstimatorFromInfo("MyAssembly", "MyFullName");
+//        }
 
-    }
-}
+//    }
+//}
