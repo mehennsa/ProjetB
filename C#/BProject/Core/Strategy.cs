efd5efefc5fff0bbb71a7e06137be094525ac582
FuchsG <guillaumfuchs@gmail.com>
2014-12-14 22:51:43 +0100

    Abstract Strategy creation

---
+++ C#/BProject/Core/Strategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    abstract class Strategy
+    {
+        List<Strategy> _dependencies;
+
+        // Renvoi un entier permettant de décrire la tendance de l'actif (achat, vente, neutre ...)
+        public abstract int Compute(IAsset asset, DateTime date);
+    }
+}
